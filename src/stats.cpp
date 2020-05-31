1bac594a302cdce764234d5c1ec9b3f06f65e85e
Keichi Takahashi <keichi.t@me.com>
2020-06-01 00:15:22 +0900

    Add stats.cpp

---
+++ src/stats.cpp
@@ -0,0 +1,25 @@
+#include "stats.hpp"
+
+namespace edm
+{
+
+float corrcoef(const TimeSeries &x, const TimeSeries &y)
+{
+#ifndef KOKKOS_ENABLE_CUDA
+    using std::min;
+    using std::sqrt;
+#endif
+
+    CorrcoefState state;
+
+    Kokkos::parallel_reduce(
+        "EDM::stats::corrcef", min(x.size(), y.size()),
+        KOKKOS_LAMBDA(int i, CorrcoefState &upd) {
+            upd += CorrcoefState(x(i), y(i));
+        },
+        Kokkos::Sum<CorrcoefState>(state));
+
+    return state.xy_m2 / sqrt(state.x_m2 * state.y_m2);
+}
+
+} // namespace edm
