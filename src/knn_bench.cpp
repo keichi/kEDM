025ac48ee4fe3278b377808db02a7f54545b47c3
Keichi Takahashi <keichi.t@me.com>
2020-10-19 16:58:53 +0900

    Switch to argh from cxxopts to support gcc 4.8

--- src/knn_bench.cpp
+++ src/knn_bench.cpp
@@ -3,33 +3,50 @@
 #include <string>
 #include <vector>
 
-#include <cxxopts.hpp>
+#include <argh.h>
 
 #include "../src/knn.hpp"
 #include "../src/types.hpp"
 
+void usage(const std::string &app_name)
+{
+    std::string msg =
+        app_name +
+        ": k-Nearest Neighbors Search Benchmark\n"
+        "\n"
+        "Usage:\n"
+        "  " +
+        app_name +
+        " [OPTION...]\n"
+        "  -l, --length arg        Length of time series (default: 10,000)\n"
+        "  -e, --embedding-dim arg Embedding dimension (default: 20)\n"
+        "  -t, --tau arg           Time delay (default: 1)\n"
+        "  -i, --iteration arg     Number of iterations (default: 10)\n"
+        "  -x, --kernel arg        Kernel type {cpu|gpu} (default: cpu)\n"
+        "  -v, --verbose           Enable verbose logging (default: false)\n"
+        "  -h, --help              Show this help";
+
+    std::cout << msg << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
-    // clang-format off
-    cxxopts::Options options("knn-bench", "Benchmark k-nearest neighbor kernel");
-    options.add_options()
-      ("e,embedding-dim", "Embedding dimension", cxxopts::value<int>()->default_value("20"))
-      ("l,length", "Length of time series", cxxopts::value<int>()->default_value("10000"))
-      ("i,iteration", "Number of iterations", cxxopts::value<int>()->default_value("10"))
-      ("t,tau", "Time delay", cxxopts::value<int>()->default_value("1"))
-      ("h,help", "Print usage");
-    // clang-format on
+    argh::parser cmdl({"-l", "--length", "-e", "--embedding-dim", "-t", "--tau",
+                       "-i", "--iteration"});
+    cmdl.parse(argc, argv);
 
-    const auto result = options.parse(argc, argv);
+    int L;
+    cmdl({"l", "length"}, 10000) >> L;
+    int E;
+    cmdl({"e", "embedding-dim"}, 20) >> E;
+    int tau;
+    cmdl({"t", "tau"}, 1) >> tau;
+    int iterations;
+    cmdl({"i", "iteration"}, 10) >> iterations;
+    int Tp = 1;
 
-    const auto L = result["length"].as<int>();
-    const auto E = result["embedding-dim"].as<int>();
-    const auto iterations = result["iteration"].as<int>();
-    const auto tau = result["tau"].as<int>();
-    const auto Tp = 1;
-
-    if (result.count("help")) {
-        std::cout << options.help() << std::endl;
+    if (cmdl[{"-h", "--help"}]) {
+        usage(cmdl[0]);
         return 0;
     }
 
