7418ba9c6abb755a849e0b676c5e00557d585e2f
Keichi Takahashi <keichi.t@me.com>
2021-01-12 17:09:33 +0900

    Split up xmap computation into three sub functions

--- src/xmap_run.cpp
+++ src/xmap_run.cpp
@@ -11,18 +11,12 @@
 #include "types.hpp"
 #include "xmap.hpp"
 
-void run(const std::string &input_path, const std::string &dataset,
-         const std::string &output_path)
+const int E_max = 20;
+const int tau = 1;
+
+void run_find_embedding_dimension(edm::Dataset ds, std::vector<int> &optimal_E,
+                                  HighFive::File &output)
 {
-    const int E_max = 20;
-    const int tau = 1;
-
-    HighFive::File output(output_path, HighFive::File::Overwrite);
-
-    const auto ds = edm::load_hdf5(input_path, dataset);
-
-    std::vector<int> optimal_E(ds.extent(1));
-
     for (size_t i = 0; i < ds.extent(1); i++) {
         Kokkos::Timer timer;
 
@@ -36,10 +30,14 @@ void run(const std::string &input_path, const std::string &dataset,
     auto dataspace = HighFive::DataSpace::From(optimal_E);
     auto ds_edim = output.createDataSet<int>("/embedding", dataspace);
     ds_edim.write(optimal_E);
+}
 
+void run_convergent_cross_mapping(edm::Dataset ds,
+                                  const std::vector<int> &optimal_E,
+                                  HighFive::File &output)
+{
     std::vector<edm::LUT> luts;
 
-    // Allocate kNN tables
     for (int E = 1; E <= E_max; E++) {
         luts.push_back(edm::LUT(ds.extent(0) - (E - 1) * tau, E + 1));
     }
@@ -50,14 +48,10 @@ void run(const std::string &input_path, const std::string &dataset,
     edm::group_ts(groups, optimal_E, E_max);
 
     edm::CrossMap ccm("ccm", ds.extent(1));
-    edm::CrossMap rho("rho", ds.extent(1));
-
     auto ccm_mirror = Kokkos::create_mirror_view(ccm);
-    auto rho_mirror = Kokkos::create_mirror_view(rho);
 
-    dataspace = HighFive::DataSpace({ds.extent(1), ds.extent(1)});
+    auto dataspace = HighFive::DataSpace({ds.extent(1), ds.extent(1)});
     auto ds_ccm = output.createDataSet<float>("/ccm", dataspace);
-    auto ds_rho = output.createDataSet<float>("/rho", dataspace);
 
     for (size_t i = 0; i < ds.extent(1); i++) {
         Kokkos::Timer timer;
@@ -72,6 +66,15 @@ void run(const std::string &input_path, const std::string &dataset,
 
         ds_ccm.select({i, 0}, {1, ds.extent(1)}).write(ccm_mirror.data());
     }
+}
+
+void run_cross_correlation(edm::Dataset ds, HighFive::File &output)
+{
+    edm::CrossMap rho("rho", ds.extent(1));
+    auto rho_mirror = Kokkos::create_mirror_view(rho);
+
+    auto dataspace = HighFive::DataSpace({ds.extent(1), ds.extent(1)});
+    auto ds_rho = output.createDataSet<float>("/rho", dataspace);
 
     for (size_t i = 0; i < ds.extent(1); i++) {
         Kokkos::Timer timer;
@@ -86,6 +89,20 @@ void run(const std::string &input_path, const std::string &dataset,
 
         ds_rho.select({i, 0}, {1, ds.extent(1)}).write(rho_mirror.data());
     }
+}
+
+void run(const std::string &input_path, const std::string &dataset,
+         const std::string &output_path)
+{
+    HighFive::File output(output_path, HighFive::File::Overwrite);
+
+    const auto ds = edm::load_hdf5(input_path, dataset);
+
+    std::vector<int> optimal_E(ds.extent(1));
+
+    run_find_embedding_dimension(ds, optimal_E, output);
+    run_convergent_cross_mapping(ds, optimal_E, output);
+    run_cross_correlation(ds, output);
 }
 
 int main(int argc, char *argv[])
