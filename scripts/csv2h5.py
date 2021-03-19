7c5765634e2cda46d5873184ea20e9bda92c2236
Keichi Takahashi <keichi.t@me.com>
2021-03-19 13:12:48 +0900

    Add logging to the Python scripts

--- scripts/csv2h5.py
+++ scripts/csv2h5.py
@@ -19,20 +19,28 @@ def main():
 
     args = parser.parse_args()
 
+    print(f"Reading from CSV file: {args.input}")
+
     header = None
     index_col = None
 
     if args.header:
+        print("Input contains a header row")
         header = 0
 
     if args.index:
+        print("Input contains a time index")
         index_col = 0
 
     df = pd.read_csv(args.input, dtype="float32", header=header,
                      index_col=index_col)
 
     if args.transpose:
+        print("Transposing input")
         df = df.T
+
+    print(f"Writing to HDF5 file: {args.input.with_suffix('.h5')}")
+    print(f"Writing {df.shape[1]} time series with {df.shape[0]} time steps")
 
     with h5py.File(args.input.with_suffix(".h5"), "w") as f:
         if args.header:
--- scripts/h52csv.py
+++ scripts/h52csv.py
@@ -13,9 +13,13 @@ def main():
 
     args = parser.parse_args()
 
+    print(f"Reading from HDF5 file: {args.input}")
+
     with h5py.File(args.input, "r") as f:
         for name, dset in f.items():
             path = args.input.with_name(f"{args.input.stem}_{name}.csv")
+
+            print(f"Writing dataset \"{name}\" to CSV file: {path}")
             np.savetxt(path, dset, delimiter=",")
 
 
