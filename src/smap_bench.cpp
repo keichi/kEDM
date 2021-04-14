13dc2b47a5b5287a524709e51b357d12e03f315b
Keichi Takahashi <keichi.t@me.com>
2021-04-14 10:43:20 +0900

    Make the S-Map benchmark parametrerized

--- src/smap_bench.cpp
+++ src/smap_bench.cpp
@@ -1,15 +1,57 @@
 #include <iostream>
 
-// #include <Kokkos_Random.hpp>
+#include <argh.h>
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Random.hpp>
 
 #include "smap.hpp"
 #include "stats.hpp"
 #include "timer.hpp"
 #include "types.hpp"
 
+void usage(const std::string &app_name)
+{
+    std::string msg =
+        app_name +
+        ": S-Map Benchmark\n"
+        "\n"
+        "Usage:\n"
+        "  " +
+        app_name +
+        " [OPTION...]\n"
+        "  -l, --length arg        Length of time series (default: 10,000)\n"
+        "  -e, --embedding-dim arg Embedding dimension (default: 20)\n"
+        "  -t, --tau arg           Time delay (default: 1)\n"
+        "  --theta arg             Localization parameter (default: 1.0)\n"
+        "  -i, --iteration arg     Number of iterations (default: 10)\n"
+        "  -v, --verbose           Enable verbose logging (default: false)\n"
+        "  -h, --help              Show this help";
+
+    std::cout << msg << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
-    const int L = 10000;
+    argh::parser cmdl({"-l", "--length", "-e", "--embedding-dim", "-t", "--tau",
+                       "--theta", "-i", "--iteration"});
+    cmdl.parse(argc, argv);
+
+    int L;
+    cmdl({"l", "length"}, 10000) >> L;
+    int E;
+    cmdl({"e", "embedding-dim"}, 20) >> E;
+    int tau;
+    cmdl({"t", "tau"}, 1) >> tau;
+    float theta;
+    cmdl({"theta"}, 1.0) >> theta;
+    int iterations;
+    cmdl({"i", "iteration"}, 10) >> iterations;
+    int Tp = 1;
+
+    if (cmdl[{"-h", "--help"}]) {
+        usage(cmdl[0]);
+        return 0;
+    }
 
     Kokkos::ScopeGuard kokkos(argc, argv);
 
@@ -19,10 +61,6 @@ int main(int argc, char *argv[])
 
     auto library_mirror = Kokkos::create_mirror_view(library);
     auto target_mirror = Kokkos::create_mirror_view(target);
-
-    // Kokkos::Random_XorShift64_Pool<> rand_pool(1931);
-    // Kokkos::fill_random(library, rand_pool, 1.0);
-    // Kokkos::fill_random(target, rand_pool, 1.0);
 
     library_mirror(0) = 0.23456f;
     target_mirror(0) = 0.34567f;
@@ -37,20 +75,17 @@ int main(int argc, char *argv[])
     Kokkos::deep_copy(library, library_mirror);
     Kokkos::deep_copy(target, target_mirror);
 
-    std::cout << "theta\trho\truntime" << std::endl;
+    Kokkos::fence();
 
-    for (float theta = 0.0f; theta < 3.0f; theta += 0.1f) {
-        Kokkos::Timer timer;
+    Kokkos::Timer timer;
 
-        edm::smap(prediction, library, target, 2, 1, 1, theta);
-
-        auto shift = (2 - 1) * 1 + 1;
-        auto shifted_target =
-            Kokkos::subview(target, Kokkos::pair<int, int>(shift, L - shift));
-
-        std::cout << theta << "\t" << edm::corrcoef(shifted_target, prediction)
-                  << "\t" << timer.seconds() << std::endl;
+    for (auto i = 0; i < iterations; i++) {
+        edm::smap(prediction, library, target, E, tau, Tp, theta);
     }
+
+    Kokkos::fence();
+
+    std::cout << timer.seconds() / iterations << " [s]" << std::endl ;
 
     return 0;
 }
