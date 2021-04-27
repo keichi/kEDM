e0f598c7ab661fdb903d6f3cdc7bce97b3548266
Keichi Takahashi <keichi.t@me.com>
2021-04-27 18:22:46 +0900

    Build wheel targeting multiple CUDA architectures

--- scripts/prepare_wheel_build_linux_gpu.sh
+++ scripts/prepare_wheel_build_linux_gpu.sh
@@ -6,18 +6,19 @@ set -ex
 pip install scikit-build cmake
 
 # Install dependencies
-yum install -y yum-utils hdf5-devel devtoolset-8
+yum install -y yum-utils hdf5-devel
 
-# Install CUDA 10.2
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel6/x86_64/cuda-rhel6.repo
-yum install -y cuda-nvcc-10-2-10.2.89-1 cuda-cudart-dev-10-2-10.2.89-1 libcublas-10-2-10.2.3.254-1 libcublas-devel-10.2.2.89-1
-ln -s /usr/local/cuda-10.2/targets/x86_64-linux/lib/stubs/libcuda.so /usr/local/cuda-10.2/targets/x86_64-linux/lib/libcuda.so.1
-ln -s /usr/local/cuda-10.2 /usr/local/cuda
+# Install CUDA 11.3
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+yum install -y cuda-nvcc-11-3-11.3.58-1 cuda-cudart-devel-11-3-11.3.58-1 libcublas-11-3-11.4.2.10064-1 libcublas-devel-11-3-11.4.2.10064-1 cuda-drivers-465.19.01-1
+ln -s /usr/local/cuda-11.3 /usr/local/cuda
 
 # Install Kokkos
 git clone https://github.com/kokkos/kokkos.git
 cd kokkos
-git checkout 3.3.00
+git checkout 3.4.00
+curl -O https://gist.githubusercontent.com/keichi/f12abe7ff807de1d6b47c9847d363b66/raw/7755aa53ddbcec9b25c691cb00949d23c67a29e1/cuda_multi_arch.diff
+patch -p1 < cuda_multi_arch.diff
 mkdir build
 cd build
 cmake \
@@ -25,9 +26,14 @@ cmake \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   -DCMAKE_CXX_COMPILER=$(pwd)/../bin/nvcc_wrapper \
   -DKokkos_ENABLE_SERIAL=ON \
-  -DKokkos_ENABLE_OPENMP=ON \
   -DKokkos_ENABLE_CUDA=ON \
   -DKokkos_ENABLE_CUDA_LAMBDA=ON \
-  -DKokkos_ARCH_VOLTA70=ON ..
+  -DKokkos_ARCH_PASCAL60=ON \
+  -DKokkos_ARCH_PASCAL61=ON \
+  -DKokkos_ARCH_VOLTA70=ON \
+  -DKokkos_ARCH_VOLTA72=ON \
+  -DKokkos_ARCH_TURING75=ON \
+  -DKokkos_ARCH_AMPERE80=ON \
+  -DKokkos_ARCH_AMPERE86=ON ..
 make
 make install
