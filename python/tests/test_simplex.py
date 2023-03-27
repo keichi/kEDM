fd6b95a712114706570d9eb1a0383c024941699f
Keichi Takahashi <hello@keichi.dev>
2023-03-28 00:30:54 +0900

    Redesigning API for Simplex projection

--- python/tests/test_simplex.py
+++ python/tests/test_simplex.py
@@ -15,7 +15,7 @@ def test_simplex(pytestconfig, E):
     library = ts[:len(ts)//2]
     target = ts[len(ts)//2-(E-1)*tau:len(ts)-(E-1)*tau]
 
-    prediction = kedm.simplex(library, target, E, tau, Tp)
+    prediction = kedm.simplex(library, target, library, E, tau, Tp)
 
     assert prediction == pytest.approx(valid, abs=1e-2)
 
@@ -32,9 +32,9 @@ def test_multivariate_simplex(pytestconfig):
     library = data[:99, [1, 4, 7]]
     target = data[97:198, [1, 4, 7]]
 
-    prediction = kedm.simplex(library, target, E, tau, Tp)
+    prediction = kedm.simplex(library, target, library[:, 0], E, tau, Tp)
 
-    assert prediction[:, 0] == pytest.approx(valid, abs=1e-6)
+    assert prediction == pytest.approx(valid, abs=1e-6)
 
 
 @pytest.mark.parametrize("E", range(1, 21))
@@ -48,14 +48,14 @@ def test_simplex_rho(pytestconfig, E):
 
     library = ts[0:100]
     target = ts[200 - (E - 1) * tau:500]
-    prediction = kedm.simplex(library, target, E, tau, Tp)
+    prediction = kedm.simplex(library, target, library, E, tau, Tp)
 
     rho = np.corrcoef(prediction[:-1], target[(E-1)*tau+Tp:])[0][1]
     rho_valid = valid[E-1]
 
     assert rho == pytest.approx(rho_valid, abs=1e-6)
 
-    rho = kedm.eval_simplex(library, target, E, tau, Tp)
+    rho = kedm.eval_simplex(library, target, library, E, tau, Tp)
 
     assert rho == pytest.approx(rho_valid, abs=1e-6)
 
@@ -65,24 +65,26 @@ def test_invalid_args():
     target = np.random.rand(10)
 
     with pytest.raises(ValueError, match=r"E must be greater than zero"):
-        kedm.simplex(library, target, E=-1)
+        kedm.simplex(library, target, library, E=-1)
 
     with pytest.raises(ValueError, match=r"tau must be greater than zero"):
-        kedm.simplex(library, target, E=2, tau=-1)
+        kedm.simplex(library, target, library, E=2, tau=-1)
 
     with pytest.raises(ValueError, match=r"Tp must be greater or equal to zero"):
-        kedm.simplex(library, target, E=2, tau=1, Tp=-1)
+        kedm.simplex(library, target, library, E=2, tau=1, Tp=-1)
 
     with pytest.raises(ValueError, match=r"library size is too small"):
-        kedm.simplex(np.random.rand(1), target)
+        kedm.simplex(np.random.rand(1), target, np.random.rand(1))
 
     with pytest.raises(ValueError, match=r"target size is too small"):
-        kedm.simplex(library, np.random.rand(1), E=2)
+        kedm.simplex(library, np.random.rand(1), library, E=2)
 
-    with pytest.raises(ValueError, match=r"library and target must have same"
+    with pytest.raises(ValueError, match=r"lib and pred must have same"
                                          r" dimensionality"):
-        kedm.simplex(np.random.rand(10), np.random.rand(10, 2))
+        kedm.simplex(np.random.rand(10), np.random.rand(10, 2),
+                     np.random.rand(10))
 
-    with pytest.raises(ValueError, match=r"library and target must be 1D or 2D"
+    with pytest.raises(ValueError, match=r"lib and pred must be 1D or 2D"
                                          r" arrays"):
-        kedm.simplex(np.random.rand(10, 2, 3), np.random.rand(10, 3, 4))
+        kedm.simplex(np.random.rand(10, 2, 3), np.random.rand(10, 3, 4),
+                     np.random.rand(10))
--- src/simplex.cpp
+++ src/simplex.cpp
@@ -6,87 +6,47 @@
 namespace edm
 {
 
-void simplex(MutableTimeSeries prediction, TimeSeries library,
+void simplex(MutableTimeSeries result, TimeSeries lib, TimeSeries pred,
              TimeSeries target, int E, int tau, int Tp)
 {
-    TmpDistances tmp("tmp_distances", target.extent(0), library.extent(0));
-    SimplexLUT lut(target.extent(0) - (E - 1) * tau, E + 1);
+    TmpDistances tmp("tmp_distances", pred.extent(0), lib.extent(0));
+    SimplexLUT lut(pred.extent(0) - (E - 1) * tau, E + 1);
 
-    knn(library, target, lut, tmp, E, tau, Tp, E + 1);
+    knn(lib, pred, lut, tmp, E, tau, Tp, E + 1);
     normalize_lut(lut);
-    lookup(prediction, library, lut);
+    lookup(result, target, lut);
 }
 
-void lookup(MutableTimeSeries prediction, TimeSeries target, SimplexLUT lut)
+void lookup(MutableTimeSeries result, TimeSeries target, SimplexLUT lut)
 {
     Kokkos::Profiling::pushRegion("EDM::simplex");
 
     const auto distances = lut.distances;
     const auto indices = lut.indices;
 
-    if (prediction.extent(0) != distances.extent(0)) {
-        throw new std::invalid_argument(
-            "prediction size and LUT row count must be equal");
-    }
-
     Kokkos::parallel_for(
         "EDM::simplex::lookup", distances.extent(0), KOKKOS_LAMBDA(int i) {
-            float pred = 0.0f;
+            float p = 0.0f;
 
             for (size_t j = 0; j < distances.extent(1); j++) {
-                pred += target(indices(i, j)) * distances(i, j);
+                p += target(indices(i, j)) * distances(i, j);
             }
 
-            prediction(i) = pred;
+            result(i) = p;
         });
 
     Kokkos::Profiling::popRegion();
 }
 
-void simplex(MutableDataset prediction, Dataset library, Dataset target, int E,
-             int tau, int Tp)
+void simplex(MutableTimeSeries result, Dataset lib, Dataset pred,
+             TimeSeries target, int E, int tau, int Tp)
 {
-    TmpDistances tmp("tmp_distances", target.extent(0), library.extent(0));
-    SimplexLUT lut(target.extent(0) - (E - 1) * tau, E + 1);
+    TmpDistances tmp("tmp_distances", pred.extent(0), lib.extent(0));
+    SimplexLUT lut(pred.extent(0) - (E - 1) * tau, E + 1);
 
-    knn(library, target, lut, tmp, E, tau, Tp, E + 1);
+    knn(lib, pred, lut, tmp, E, tau, Tp, E + 1);
     normalize_lut(lut);
-    lookup(prediction, library, lut);
-}
-
-void lookup(MutableDataset prediction, Dataset target, SimplexLUT lut)
-{
-    Kokkos::Profiling::pushRegion("EDM::simplex");
-
-    const auto distances = lut.distances;
-    const auto indices = lut.indices;
-
-    if (prediction.extent(0) != distances.extent(0)) {
-        throw new std::invalid_argument(
-            "prediction time steps and LUT row count must be equal");
-    }
-
-    // Loop over time steps
-    Kokkos::parallel_for(
-        "EDM::simplex::lookup", distances.extent(0), KOKKOS_LAMBDA(int i) {
-            // Loop over time series
-            for (size_t k = 0; k < target.extent(1); k++) {
-                prediction(i, k) = 0.0f;
-            }
-
-            // Loop over embedding dimensions
-            for (size_t j = 0; j < distances.extent(1); j++) {
-                float weight = distances(i, j);
-                int index = indices(i, j);
-
-                // Loop over time series
-                for (size_t k = 0; k < target.extent(1); k++) {
-                    prediction(i, k) += target(index, k) * weight;
-                }
-            }
-        });
-
-    Kokkos::Profiling::popRegion();
+    lookup(result, target, lut);
 }
 
 } // namespace edm
