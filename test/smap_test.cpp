cdc9519720f7e3798b2fa5b57d06e98db19e3e6c
Keichi Takahashi <hello@keichi.dev>
2023-04-11 21:42:27 +0900

    Update S-map value tests

--- test/smap_test.cpp
+++ test/smap_test.cpp
@@ -11,31 +11,29 @@ namespace edm
 TEST_CASE("Compute S-Map projections")
 {
     const auto E = 2, tau = 1, Tp = 1;
-    const auto L = 100;
+    std::vector<float> thetas = {0.0, 0.01, 0.1, 0.3, 0.5, 0.75, 1, 2,
+                                 3,   4,    5,   6,   7,   8,    9};
 
     const auto ds1 = load_csv("logistic_map.csv");
-    const auto lib = TimeSeries(ds1, Kokkos::pair<int, int>(0, L), 0);
-    const auto pred = TimeSeries(ds1, Kokkos::pair<int, int>(L, L * 2), 0);
-    const auto result = MutableTimeSeries("result", L);
+    const auto lib = TimeSeries(ds1, Kokkos::ALL, 1);
+    const auto pred = TimeSeries(ds1, Kokkos::ALL, 1);
+    const auto result =
+        MutableTimeSeries("result", pred.extent(0) - (E - 1) * tau);
 
-    const auto ds2 = load_csv("logistic_map_validation.csv");
+    const auto ds2 = load_csv("smap_valid_logistic_map.csv");
     const auto ds2_mirror =
         Kokkos::create_mirror_view_and_copy(HostSpace(), ds2);
 
-    for (size_t i = 0; i < ds2_mirror.extent(0); i++) {
-        const auto theta = ds2_mirror(i, 0);
+    for (size_t i = 0; i < thetas.size(); i++) {
+        smap(result, lib, pred, pred, E, tau, Tp, thetas[i]);
 
-        smap(result, lib, pred, pred, E, tau, Tp, theta);
+        const auto result_mirror =
+            Kokkos::create_mirror_view_and_copy(HostSpace(), result);
 
-        const auto shift = (E - 1) * tau + Tp;
-        const auto shifted_pred =
-            TimeSeries(pred, Kokkos::pair<int, int>(shift, L));
-
-        // TODO Need to check why the test fails without this fence
-        Kokkos::fence();
-
-        CHECK(corrcoef(shifted_pred, result) ==
-              doctest::Approx(ds2_mirror(i, 1)).epsilon(0.01f));
+        for (size_t j = 0; j < result_mirror.extent(0); j++) {
+            CHECK(result_mirror(j) ==
+                  doctest::Approx(ds2(j, i + 1)).epsilon(5e-2f));
+        }
     }
 }
 
